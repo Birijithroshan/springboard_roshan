8c379f356f1c38b35c168033db77ec5a3efe9236
Birijith Roshan <birijithroshan29@gmail.com>
2025-10-08 14:18:17 +0530

    updated code

---
+++ src/main/java/org/automation/AutomationApplication.java
@@ -0,0 +1,15 @@
+package org.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application class for the Automation Framework
+ */
+@SpringBootApplication
+public class AutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomationApplication.class, args);
+    }
+}
